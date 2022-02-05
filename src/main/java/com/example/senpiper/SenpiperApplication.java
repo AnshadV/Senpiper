dea67fbec69145ad6bba0a07397d2923fba92640
AnshadV <anshadvaz@gmail.com>
2022-02-05 14:38:18 +0530

    Initial commit

---
+++ src/main/java/com/example/senpiper/SenpiperApplication.java
@@ -0,0 +1,13 @@
+package com.example.senpiper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenpiperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SenpiperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/senpiper/SenpiperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.senpiper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenpiperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
